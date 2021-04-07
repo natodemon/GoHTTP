cffa7ea5ebbf569b0628972fad804ccb0b6f0618
natodemon <aes17optimus@gmail.com>
2021-04-07 14:02:43 +0200

    Http server & basic readme complete

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GoHTTP
+
+## Purpose
+
+This is a very simple http server written in Golang as a component of another project, please see [natodemon/GoBench/README.md](https://github.com/natodemon/GoBench). It simply serves an html index file placed in the *web* directory.
+
+## Usage
+
+    go run httpserver.go [-p=_port_]
+    ./httpserver [-p=_port_]
+
+If no port is provided, the default is 8080
--- httpserver.go
+++ httpserver.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 func main() {
-	fmt.Println("Test")
+	portNo := flag.Int("p", 8080, "port to listen for requests")
+
+	flag.Parse()
+
+	router := http.NewServeMux()
+
+	htServer := http.FileServer(http.Dir("./web"))
+	router.Handle("/", htServer)
+
+	println("HTTP server started on port", *portNo)
+
+	var pString string = ":" + strconv.Itoa(*portNo)
+	log.Fatal(http.ListenAndServe(pString, requestLogger(router)))
+}
+
+func requestLogger(hand http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqTime := time.Now()
+
+		hand.ServeHTTP(w, r)
+
+		fmt.Printf("%v\t\t%s\t%s\t%s\t\n", reqTime.Format(time.Stamp), r.RequestURI, r.Method, r.RemoteAddr)
+	})
 }
